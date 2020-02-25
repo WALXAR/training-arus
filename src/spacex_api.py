91f3df247377c5dbcdea3cef8c9d4e39d2ff0d54
David Lopes <david.lopes@dynatrace.com>
2020-02-25 09:39:35 -0300

    Version 1.003

--- src/spacex_api.py
+++ src/spacex_api.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def get_ships():
-    url = "http://ec2-18-229-131-87.sa-east-1.compute.amazonaws.com/v3/ships"
+def get_ships(base_url):
+    url = f"{base_url}/v3/ships"
     r = requests.get(url)
     return r.json()
 
@@ -11,5 +11,5 @@ def main():
     print(len(get_ships()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/spacex_plugin.py
+++ src/spacex_plugin.py
@@ -8,9 +8,11 @@ logger = logging.getLogger(__name__)
 
 
 class SpaceXPlugin(RemoteBasePlugin):
-
     def query(self, **kwargs) -> None:
-        ships = get_ships()
+
+        config = kwargs.get("config")
+
+        ships = get_ships(config.get("url"))
         logger.info(f"Encontré {len(ships)} navios")
 
         group = self.topology_builder.create_group("Ships", "Ships")
@@ -19,8 +21,3 @@ class SpaceXPlugin(RemoteBasePlugin):
             logger.info(f"Procesando navio {ship['ship_name']}")
             device = group.create_device(ship["ship_id"], ship["ship_name"])
             device.absolute("fuel", ship["fuel"])
-
-
-
-
-
