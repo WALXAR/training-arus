b4bf08123895721ef0fabcf3cac682bdb463c8fa
David Lopes <david.lopes@dynatrace.com>
2020-02-25 10:26:27 -0300

    Version 1.007

--- src/spacex_plugin.py
+++ src/spacex_plugin.py
@@ -21,9 +21,18 @@ class SpaceXPlugin(RemoteBasePlugin):
             logger.info(f"Procesando navio {ship['ship_name']}")
             device = group.create_device(ship["ship_id"], ship["ship_name"])
 
+            # Absolute metric - Value AS IS
             device.absolute("fuel", ship["fuel"])
 
             for engine in ship["thrust"]:
+
+                # Dimensions (1 Custom Metric per dimension)
                 device.absolute("thrust", engine["power"], dimensions={"Motor": engine["engine"]})
 
+            # Topology data
             device.add_endpoint(ship["ship_ip"])
+
+            # Properties
+            device.report_property("Puerto", ship.get("home_port", "Desconocido"))
+            device.report_property("Activo", str(ship.get("active", True)))
+            device.report_property("Construcción", str(ship.get("year_built", "Desconocido")))
